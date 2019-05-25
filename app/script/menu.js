25fe5dc8d8598ce938ff728af350bead77944a77
diedmoons <dianond707@gmail.com>
2019-05-25 14:27:42 +0300

    Added btn-top

---
+++ app/script/menu.js
@@ -0,0 +1,11 @@
+window.onload  = function () {
+    var menuStyle = getComputedStyle(menu);
+    openMenu.onclick = function () {
+        if (menuStyle.display == "none"){
+            menu.classList.add("active");
+        }
+        else {
+            menu.classList.remove("active");
+        }
+    }
+}
